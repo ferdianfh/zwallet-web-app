f2d5cc1f301fc741a19b75d07d66beef8b2d23ab
ferdif <ferdifh82@gmail.com>
2022-03-29 14:52:28 +0700

    mobile layout (max-width: 628px): add new phone page

--- src/pages/Apps/NewPhone/index.js
+++ src/pages/Apps/NewPhone/index.js
@@ -12,6 +12,7 @@ import "./newphone.css";
 import { useDispatch, useSelector } from "react-redux";
 import { NewPhoneNumber } from "../../../redux/actions/apps/addPhoneNumber";
 import { GetProfile } from "../../../redux/actions/apps/getProfile";
+import Sidebar from "../../../components/module/Sidebar";
 
 const NewPhone = () => {
   const dispatch = useDispatch();
@@ -47,54 +48,93 @@ const NewPhone = () => {
 
   return (
     <Fragment>
-      <section className="content-bar big-screen col-lg-8 animation-pull-out ">
-        <section className="new-phone-content d-flex flex-column">
-          <div className="new-phone-text">
-            <p className="new-phone-title">Add Phone Number</p>
-            <p className="new-phone-desc">
-              Add at least one phone number for the transfer <br /> ID so you
-              can start transfering your money to <br /> another user.
-            </p>
-          </div>
-          <div className="form-new-phone d-flex flex-column justify-content-center align-items-center">
-            <div className="input-form pass d-flex mt-5">
-              <BsIcons.BsTelephone className="form-icons position-absolute" />
-              <span className=" phone-code position-absolute ">+62</span>
-              <Input
-                value={form.phone}
-                onChange={handleChange}
-                name="phone"
-                className="input-new-phone"
-                placeholder="Enter your phone number"
-                type="text"
-              />
-            </div>
+      <div className="small-screen-content d-lg-none animation-pull-out">
+        <section className="personalInfoContentSm d-flex flex-column justify-content-center align-items-center d-lg-none">
+          <p className="newPhoneDesc">
+            Add at least one phone number for the transfer ID so you can start
+            transfering your money to another user.
+          </p>
 
-            {errorMessage ? (
-              <p className="text-error mb-0">{errorMessage}</p>
-            ) : null}
-
-            <div className="btn-new-phone d-flex align-items-center mt-0">
-              <Button
-                isLoading={newPhoneData.loading}
-                onClick={handleClick}
-                className="button btn-login btn-add-phone"
-              >
-                Add Phone Number
-              </Button>
-            </div>
-          </div>
-
-          {openModalSuccess ? (
-            <ModalSuccess
-              successTitle="Add Phone Number Success!"
-              successDesc={`Phone number +62 ${form.phone} successfully added to your profile. Now you can start transactions!`}
-              action="Go back to Profile"
-              closeModal={handleNavigate}
+          <div className="input-form pass d-flex mt-5">
+            <BsIcons.BsTelephone className="form-icons position-absolute" />
+            <span className=" phone-code position-absolute ">+62</span>
+            <Input
+              value={form.phone}
+              onChange={handleChange}
+              name="phone"
+              className="input-new-phone"
+              placeholder="Enter your phone number"
+              type="text"
             />
+          </div>
+
+          {errorMessage ? (
+            <p className="text-error mb-0">{errorMessage}</p>
           ) : null}
+
+          <div className="btn-new-phone d-flex align-items-center">
+            <Button
+              isLoading={newPhoneData.loading}
+              onClick={handleClick}
+              className="button btn-login btn-add-phone"
+            >
+              Add Phone Number
+            </Button>
+          </div>
         </section>
-      </section>
+      </div>
+
+      <div className="big-screen-content d-none d-lg-block d-lg-flex mt-lg-2">
+        <Sidebar />
+
+        <section className="content-bar big-screen col-lg-8 animation-pull-out ">
+          <section className="new-phone-content d-flex flex-column">
+            <div className="new-phone-text">
+              <p className="new-phone-title">Add Phone Number</p>
+              <p className="new-phone-desc">
+                Add at least one phone number for the transfer <br /> ID so you
+                can start transfering your money to <br /> another user.
+              </p>
+            </div>
+            <div className="form-new-phone d-flex flex-column justify-content-center align-items-center">
+              <div className="input-form pass d-flex mt-5">
+                <BsIcons.BsTelephone className="form-icons position-absolute" />
+                <span className=" phone-code position-absolute ">+62</span>
+                <Input
+                  value={form.phone}
+                  onChange={handleChange}
+                  name="phone"
+                  className="input-new-phone"
+                  placeholder="Enter your phone number"
+                  type="text"
+                />
+              </div>
+
+              {errorMessage ? (
+                <p className="text-error mb-0">{errorMessage}</p>
+              ) : null}
+
+              <div className="btn-new-phone d-flex align-items-center mt-0">
+                <Button
+                  isLoading={newPhoneData.loading}
+                  onClick={handleClick}
+                  className="button btn-login btn-add-phone"
+                >
+                  Add Phone Number
+                </Button>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+      {openModalSuccess ? (
+        <ModalSuccess
+          successTitle="Add Phone Number Success!"
+          successDesc={`Phone number +62 ${form.phone} successfully added to your profile.`}
+          action="Go back to Profile"
+          closeModal={handleNavigate}
+        />
+      ) : null}
     </Fragment>
   );
 };
